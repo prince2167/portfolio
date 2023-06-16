0a65d6c14c3bf78dcff2f1cbc484edd469d4063d
Prince <singhprince8222692@gmail.com>
2023-06-16 18:30:13 +0530

    add projects data

---
+++ src/utils/projectData.js
@@ -0,0 +1,26 @@
+import { clothify, noteTaking } from "../assets/images/index";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Clothify",
+    description:
+      "Clothify is your go-to fashion e-commerce platform. The ultimate fashion e-commerce platform. Discover the latest trends and stylish outfits to elevate your wardrobe.",
+    image: clothify,
+    frameWork: "React",
+    css: "Vanilla CSS",
+    liveLink: "https://prince-clothify.vercel.app/",
+    githubLink: "https://github.com/prince2167/e-commerce",
+  },
+  {
+    id: 2,
+    title: "Note-Taking",
+    description:
+      "Note taking- The easy notebook for notes, to-do lists and grocery list with call ID. Powerful notepad to capture ideas and save notes.",
+    image: noteTaking,
+    frameWork: "React",
+    css: "Vanilla CSS",
+    liveLink: "https://note-taking-app-five.vercel.app/",
+    githubLink: "https://github.com/prince2167/Note-taking",
+  },
+];
