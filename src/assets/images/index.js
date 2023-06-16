eff25a45a9622e1886cd995ae427c5312af77f24
Prince <singhprince8222692@gmail.com>
2023-06-16 18:24:05 +0530

    add images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import portfolio from "./portfolio.jpeg";
 import project from "./projectImage.gif";
 import blog from "./blogImage.gif";
-
-export { portfolio, project, blog };
+import clothify from "./clothify.png";
+import noteTaking from "./note-taking.png";
+export { portfolio, project, blog, clothify, noteTaking };
